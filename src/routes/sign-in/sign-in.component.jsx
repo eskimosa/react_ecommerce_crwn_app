8ceea70b2d8b90420930889531d8d8df0d12963c
eskimosa <esskimosa@gmail.com>
2024-04-15 18:25:29 -0500

    worked on creating sign up form component and adding functionality

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,24 +1,19 @@
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithRedirect } from "../../utils/firebase/firebase.utils";
-import { useEffect } from "react";
-import { getRedirectResult } from 'firebase/auth';
-
+import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
 
 const SignIn = () => {
-    useEffect(async() => {
-        const response = await getRedirectResult(auth);
-        console.log(response)
-    }, []);
 
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     };
 
+
     return (
         <div>
             <h1>Sign In Page</h1>
             <button onClick={logGoogleUser}>Sign In with Google Popup</button>
-            <button onClick={signInWithRedirect}>Sign In with Google Redirect</button>
+            <SignUpForm />
         </div>
     );
 };
