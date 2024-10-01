1078730f0b11d8f24b7a725a3419ae85452e0bf6
eskimosa <esskimosa@gmail.com>
2024-10-01 08:41:02 +0200

    worked on creating and dispatching actions for user redux

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,4 +1,5 @@
-import { createContext, useEffect, useReducer } from 'react';
+import { useEffect } from 'react';
+import { useReducer, createContext } from 'react';
 
 import {
   onAuthStateChangedListener,
